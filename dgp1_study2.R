948d321dcf59d665a59beef8beb6113a75e2e13c
George Perrett <49445400+gperrett@users.noreply.github.com>
2025-08-27 15:52:34 -0400

    Update dgp1_study2.R

--- dgp1_study2.R
+++ dgp1_study2.R
@@ -19,7 +19,7 @@ for (i in 1:8) {
 
 x_mat <- apply(x_mat, 2, as.double)
 # grid of subgroup effect sizes
-d <- seq(0.1, 1.5, .1) 
+d <- seq(0, 1.5, .1) 
 
 run <- lapply(1:length(d), function(i){
   set.seed(2)
@@ -27,7 +27,7 @@ run <- lapply(1:length(d), function(i){
   y0hat <- rep(0, n)
   
   # note everything is 0 expet for group 8 which is set to d
-  beta <- rnorm(8, 0, d[i])
+  beta <- c(rep(0, 4), rep(d[i], 4))
   # assign subgroup effect for group 8
   y1hat <- as.vector(x_mat%*%beta)
   
@@ -56,18 +56,26 @@ run <- lapply(1:length(d), function(i){
   fit2 <- dbarts::bart2(y ~ z + as.factor(x), data = dat, test = test_df)
   fit3 <- stochtree::bcf(X_train = x_mat, Z_train = z, y_train = y, num_mcmc = 1000)
   fit4 <- rstanarm::stan_glm(y ~ z*as.factor(x), data = dat)
+  s4b_dat <- dat
+  s4b_test <- test_df
+  s4b_test$x <- as.factor(test_df$x)
+  s4b_dat$x <- as.factor(s4b_dat$x)
+  fit5 <- stan4bart::stan4bart(y ~ bart(z + x) + (1 + z|x), data = s4b_dat, test = s4b_test)
   
   # extract icate (we'll need this for SVD)
   icate_fit1 <- compute_icate(fit1, test = test_df)
   icate_fit2 <- compute_icate(fit2, test = test_df)
   icate_fit3 <- compute_icate(fit3)
   icate_fit4 <- compute_icate(fit4, test = test_df)
+  icate_fit5 <- compute_icate(fit5, test = test_df)
+  
   
   # save as a list for later
   icate <- list(mlm_icate = icate_fit1, 
                 BART_icate = icate_fit2, 
                 bcf_icate = icate_fit3, 
-                lm_icate = icate_fit4)
+                lm_icate = icate_fit4, 
+                s4b = icate_fit5)
   
   # extract statistics
   subgroup_effects <- rbind(
@@ -101,23 +109,26 @@ run <- lapply(1:length(d), function(i){
       icate = icate_fit4,
       dat = dat,
       model = 'lm'
+    ), 
+    
+    compute_subgroup_effects(
+      n_groups = 8,
+      cate_truth = cate_truth,
+      icate = icate_fit5,
+      dat = dat,
+      model = 'stan4bart'
     )
   )
   
   # keep track of iteration and d for book keeping 
   subgroup_effects$iter <- iter
   subgroup_effects$d <- d[i]
-  
-  list(subgroup = subgroup_effects, icate = icate)
-  
+  subgroup_effects
+
 })
 
 
-export_subgroups <- do.call('rbind', lapply(run, '[[', 1))
-export_icates <- lapply(run, '[[', 2)
+export_subgroups <- do.call('rbind', run)
 
-path <- paste0('./results/subgroups_dgp1_study2/results', iter, '.csv')
+path <- paste0('./results/subgroups_dgp2_study1/results', iter, '.csv')
 readr::write_csv(export_subgroups, path)
-
-path <- paste0('./results/icates_dgp1_study2/results', iter, '.rds')
-readr::write_rds(export_icates, path)
